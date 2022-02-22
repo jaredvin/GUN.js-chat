cbf94f70d55a8c936153bd9a3c6c954a5814bde3
jaredvin <98550767+jaredvin@users.noreply.github.com>
2022-02-22 18:37:42 -0500

    Add files via upload

---
+++ src/Login.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { user } from './user';
+
+    let username;
+    let password;
+
+</script>
+
+<label for= "username">Username</label>
+<input name="username" bind:value={username} minlength="3" maxlength="16" />
+
+<label for="password">Password</label>
+<input name="password" bind:value={password} type="password" />
--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+import App from './App.svelte';
 
+const app = new App({
+	target: document.body,
+});
+
+export default app;
